8285eaf4b005306023aa48beeec7ed804b09ee68
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-06-02 21:37:22 +0200

    Add the random advice from computer project

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const options = ['take it', 'leave it'];
+
+const btnAdd = document.querySelector('.add');
+const btnClean = document.querySelector('.clean');
+const input = document.querySelector('input');
+const btnAdvice = document.querySelector('.showAdvice');
+const btnOptions = document.querySelector('.showOptions');
+const h1 = document.querySelector('h1');
+
+
+const addOptions = (e) => {
+  e.preventDefault();
+  options.push(input.value);
+  
+  if (input.value.length) {
+    alert(options);
+    input.value = "";
+  }
+}
+
+const cleanOptions = (e) => {
+  e.preventDefault();
+  options.length = 0;  //only if we want to remove all items from the array
+  //h1.textContent = '';
+  //input.value = "";
+}
+
+const showAdvice = (e) => {
+  const index = Math.floor(Math.random() * options.length);
+  console.log(index);
+  h1.textContent = options[index];
+}
+
+const showOptions = (e) => {
+  alert(options.join(', '));
+}
+
+btnAdd.addEventListener('click', addOptions);
+btnClean.addEventListener('click', cleanOptions);
+btnAdvice.addEventListener('click', showAdvice);
+btnOptions.addEventListener('click', showOptions);
